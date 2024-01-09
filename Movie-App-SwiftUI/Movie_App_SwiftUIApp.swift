554353611a53b474716fd8e9658e5d488f4b298c
Ayman-Naim <mnameno_2012@yahoo.com>
2024-01-09 08:28:59 +0200

    Initial Commit

---
+++ Movie-App-SwiftUI/Movie_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_App_SwiftUIApp.swift
+//  Movie-App-SwiftUI
+//
+//  Created by ayman on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
